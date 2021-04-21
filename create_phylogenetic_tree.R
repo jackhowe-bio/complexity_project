b1ebd049313826b3756274458704ed6d960151d0
jack howe <jack.howe@zoo.ox.ac.uk>
2021-04-21 15:14:29 +0100

    made the animal tree work, 2 could not be found, cyanea in wrong place

--- create_phylogenetic_tree.R
+++ create_phylogenetic_tree.R
@@ -4,87 +4,16 @@
 library(ape)
 library(ggplot2)
 library(brms)
+library(rotl) #see https://cran.r-project.org/web/packages/rotl/vignettes/rotl.html
+
+
+df<- read.csv('germline_data_1.0.csv')
 
 # create tree
 
-
-myTreeAnimals <- read.tree(text='
-( 
-
-  (
-    (Hirudo medicinalis,
-    Lumbricus terrestris,
-    Nais variabilis,
-    Dinophilus gyrociliatus,
-    Diurodrilus westheidei,
-    apodotrocha progenerans,
-    Aeolosoma tenebrarum,
-    Dasybranchus caducus,
-    Pisione remota,
-    Pomatoceros triqueter),
-  
-    (Periplaneta americana,
-    Callinectes sapidus),
-  )
-  
-  pycnophyes frequens,
-
-  (Morone saxatilis,
-  Salmo gairdneri,
-  Canis familiaris,
-  Mus musculus),
-
-  (Hydra vulgaris,
-  microhydra rideri,
-  cyanea cyanea,
-  haliclystus haliclystus),
-
-  (Pleurobrachia),
-
-  (conocyema polymorpha,
-  dicyema typhus,
-  dicyemmenea abelis,
-  dicyemmenea lameerei),
-
-  (turbanella cornuta,
-  chordodasys antennatus),
-
-  (valvognathia pogonostoa,
-  rastrognathhia macrostoma),
-
-  (loxosoma sultana,
-  pedicillina echinata),
-
-  (amphibola crenata,
-  neomenia carinata),
-
-  (caenorhabditis elegans,
-  rhabditis monhystera,)
-
-  (rhopalura granosa),
-  
-  (Trichoplax adhaerens),
-  
-  macrostomum gigas,
-  entersostomula graffi,
-  dugesia mediterranea,
-  spongilla lacustris,
-  notholca acuminata,
-  apsilus vorax,
-  anaperus sulcatus
-  
-
-  
-);')
-plot(myTreeAnimals)
-
-
-library(rotl)
-taxa <- c("Hyla", "Salmo", "Diadema", "Nautilus")
-resolved_names <- tnrs_match_names(taxa)
-
-df<- read.csv('germline_data_1.0.csv')
+#animals
 animals <- subset(df, kingdom == 'Animalia')
-resolved_names_animals <- tnrs_match_names(animals$Species, context_name = 'Animals')
+ResolvedNamesAnimals <- tnrs_match_names(animals$species.updated, context_name = 'Animals')
 
-
+AnimalTree<- tol_induced_subtree(ResolvedNamesAnimals$ott_id[-c(33,41)])
+plot(AnimalTree, no.margin = TRUE)
