69bc4bc2ca6195fda96528e75aa398a6a7fc54f6
Jack Howe <j.howe1992@gmail.com>
2021-10-18 17:37:53 +0200

    written bash file to run all scripts

---
+++ run_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p RScripts/model_outputs
+mkdir -p figures
+RScript RScripts/OptimisingModels.R
+RScript RScripts/ParameterDefinitions.R
+RScript RScripts/Model1.R
+RScript RScripts/Model2.R
+RScript RScripts/Model3.R
+RScript RScripts/Model4.R
+RScript RScripts/Model5.R
+RScript RScripts/Model6.R
+RScript RScripts/Model7.R
+RScript RScripts/Model8.R
