f04cc6904d5ea1cc2e90ee86582dff0ebb6cd33d
Jack Howe <j.howe1992@gmail.com>
2023-03-24 11:26:33 +0100

    RScripts folder restructured

--- RScripts/ParameterDefinitions.R
+++
@@ -1,9 +0,0 @@
-iterations<- 8000000
-burnin<- 1000000
-thinning <- 100
-n_chains<- 6
-
-saveRDS(iterations, file = 'RScripts/iterations.RDS')
-saveRDS(burnin, file = 'RScripts/burnin.RDS')
-saveRDS(thinning, file = 'RScripts/thinning.RDS')
-saveRDS(n_chains, file = 'RScripts/n_chains.RDS')
